ac21af8880223d9dff8add09334cc85326af9df8
Valentyna <pr.valya1989@gmail.com>
2026-03-05 18:55:40 +0200

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,25 @@
+"use strict";
+const ul = document.querySelector("ul");
+const list = [
+    ...document.querySelectorAll("li")
+];
+function getNumber(element) {
+    return Number(element.dataset.salary.replace(/[$,]/g, ""));
+}
+function sortList(li, unList) {
+    li.sort((el1, el2)=>getNumber(el2) - getNumber(el1));
+    unList.append(...li);
+    return li;
+}
+function getEmployees(li) {
+    return li.map((el)=>({
+            name: el.innerText,
+            position: el.dataset.position,
+            salary: el.dataset.salary,
+            age: Number(el.dataset.age)
+        }));
+}
+sortList(list, ul);
+getEmployees(list);
+
+//# sourceMappingURL=index.f75de5e1.js.map
