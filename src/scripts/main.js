bf694103753cb577bc806aa6084b9838e7728c6a
Valentyna <pr.valya1989@gmail.com>
2026-03-05 18:20:50 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// write code here
+const ul = document.querySelector('ul');
+const list = [...document.querySelectorAll('li')];
+
+function sortList(li) {
+  return li.sort((el1, el2) => {
+    const salaryEl1 = Number(el1.dataset.salary.replace(/[$,]/g, ''));
+    const salaryEl2 = Number(el2.dataset.salary.replace(/[$,]/g, ''));
+
+    return salaryEl2 - salaryEl1;
+  });
+}
+
+const sorted = sortList(list);
+
+ul.append(...sorted);
+
+function getEmployees(li) {
+  return li.map((el) => ({
+    name: el.innerText,
+    position: el.dataset.position,
+    salary: el.dataset.salary,
+    age: Number(el.dataset.age),
+  }));
+}
+
+sortList(list);
+getEmployees(list);
